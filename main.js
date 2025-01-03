dfc8563271e9258d297dae8666c664cc49d2f4de
AlanC7 <81393852+AlanC7@users.noreply.github.com>
2025-01-02 23:00:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const boton = document.querySelector("#botonmenu");
+/*const boton = document.querySelector("#botonmenu");
 const contenedormenu = document.querySelector("#contemenu");
 const lista = document.querySelector("#menu");
 
@@ -26,6 +26,47 @@ boton.addEventListener("click", () => {
         boton.classList.add('bi-list');
     }
     
+});*/
+
+
+const boton = document.querySelector("#botonmenu");
+const contenedormenu = document.querySelector("#contemenu");
+const lista = document.querySelector("#menu");
+
+boton.addEventListener("click", () => {
+    contenedormenu.classList.toggle("visiblecontenedor"); 
+    if (lista.classList.contains("visiblemenu")) {
+        lista.classList.remove("visiblemenu");
+        lista.style.display = "none"; 
+    } else {  
+        lista.classList.add("visiblemenu"); 
+        lista.style.display = "block"; 
+        lista.style.flexDirection = "column";
+    }
+
+    if (boton.classList.contains('bi-list')) {
+        boton.classList.remove('bi-list');
+        boton.classList.add('bi-x-circle-fill');
+    } else {
+        boton.classList.remove('bi-x-circle-fill');
+        boton.classList.add('bi-list');
+    }
 });
+
+// Asegúrate de que el menú se muestre correctamente al cambiar el tamaño de la pantalla
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1100) { // Ajusta el valor según tus necesidades
+        lista.style.display = "flex";
+        lista.style.flexDirection = "row";
+        lista.classList.remove("visiblemenu");
+        contenedormenu.classList.remove("visiblecontenedor");
+        boton.classList.remove('bi-x-circle-fill');
+        boton.classList.add('bi-list');
+    } else {
+        lista.style.display = "none";
+    }
+});
+
+
 
 
