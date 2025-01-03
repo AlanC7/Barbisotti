1b8d56616a32c4593bd885c84c4623b71fd4316b
AlanC7 <81393852+AlanC7@users.noreply.github.com>
2025-01-02 21:17:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-/*const boton =document.querySelector("#botonmenu");
-const contenedormenu =document.querySelector("#contemenu");
- const lista =document.querySelector("#menu");
- boton.addEventListener("click",() =>{
-     contenedormenu.classList.add("visiblecontenedor");
-     lista.classList.add("visiblemenu");
-})
 
-boton.addEventListener("click",() =>{
-    contenedormenu.classList.remove("visiblecontenedor");
-    lista.classList.remove("visiblemenu");
-})*/
 const boton = document.querySelector("#botonmenu");
 const contenedormenu = document.querySelector("#contemenu");
 const lista = document.querySelector("#menu");
 
 boton.addEventListener("click", () => {
     contenedormenu.classList.toggle("visiblecontenedor");
-    lista.classList.toggle("visiblemenu");
+    lista.classList.toggle("visiblemenu"); 
+    if (lista.classList.contains("visiblemenu"))
+    {
+        lista.style.display="block";
+    }
+    else{ lista.style.display="none";}
+
+    if (boton.classList.contains('bi-list')) {
+        boton.classList.remove('bi-list');
+        boton.classList.add('bi-x-circle-fill');
+    } else {
+        boton.classList.remove('bi-x-circle-fill');
+        boton.classList.add('bi-list');
+    }
 });
 
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const botonMenu = document.getElementById('botonmenu');
-//     const contenedorMenu = document.getElementById('contemenu');
-//     const menu = document.getElementById('menu');
-
-//     botonMenu.addEventListener('click', function() {
-//         contenedorMenu.classList.toggle('visiblecontenedor');
-//         menu.classList.toggle('visiblemenu');
-//     });
-// });
 
